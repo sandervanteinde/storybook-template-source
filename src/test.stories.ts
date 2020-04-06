7417d6e58b34efe89599d81c1169b8b20483a555
Sander van t Einde <sandervanteinde@gmail.com>
2020-04-06 09:52:41 +0200

    Update dependencies

--- src/test.stories.ts
+++ src/test.stories.ts
@@ -1,10 +1,10 @@
-import { storiesOf } from '@storybook/html';
-import { withTemplateSource } from '.';
+import { withTemplateSource } from './with-template-source';
 
-storiesOf('Test Stories', module)
-    .add('With template', () => withTemplateSource('<h1>Hallo Wereld</h1>'))
-    .add('Without template', () => '<h1>Hallo Wereld</h1>')
-    .add('A lot of HTML without pretty print', () => withTemplateSource(`<!DOCTYPE html>
+export default { title: 'Test Stories' }
+
+export const withTemplate = () => withTemplateSource('<h1>Hallo Wereld</h1>');
+export const withoutTemplate = () => '<h1>Hallo Wereld</h1>';
+export const aLotOfHTMLWithoutPrettyPrint = () => withTemplateSource(`<!DOCTYPE html>
     <html lang="en">
     
     <head>
@@ -18,16 +18,25 @@ storiesOf('Test Stories', module)
         Awesomeness
     </body>
     
-    </html>`, { pretty: false }))
-    .add('Angular template with pretty print', () => withTemplateSource(`
-    <form [formGroup]="form" style="width: 500px">
-        <rosa-form-group>
-                    <label *ngIf="label" rosaFormLabel>{{label}}</label>
-            <rosa-input [readonly]="isReadonly" formControlName="test" placeholder="This input requires 5 characters">
-                       <i *ngIf="showIcon" rosaInputIcon class="icon-briefcase"></i>
-                <span [rosaInputAddonLabel]="labelsFullCaps ? 'FullCaps' : 'Default'" *ngFor="let label of labels">{{label}}</span>
-            </rosa-input>
-                 <p rosaValidationMessage *ngFor="let validationMessage of validationMessages">{{validationMessage}}</p>
-        </rosa-form-group>
-    </form>
-    `))
+    </html>`, { pretty: false });
+export const angularTemplateWithPrettyPrint = () => withTemplateSource(`
+<form [formGroup]="form" style="width: 500px">
+    <rosa-form-group>
+                <label *ngIf="label" rosaFormLabel>{{label}}</label>
+        <rosa-input [readonly]="isReadonly" formControlName="test" placeholder="This input requires 5 characters">
+                    <i *ngIf="showIcon" rosaInputIcon class="icon-briefcase"></i>
+            <span [rosaInputAddonLabel]="labelsFullCaps ? 'FullCaps' : 'Default'" *ngFor="let label of labels">{{label}}</span>
+        </rosa-input>
+              <p rosaValidationMessage *ngFor="let validationMessage of validationMessages">{{validationMessage}}</p>
+    </rosa-form-group>
+</form>
+`);
+
+export const withVeryLongOneLiners = () => withTemplateSource(`
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget metus quis purus congue dapibus. Integer ac tincidunt est. Integer porttitor accumsan dignissim. Integer vitae erat sed mi pulvinar viverra. Vivamus bibendum posuere egestas. Sed aliquet eros magna, at blandit eros lobortis sit amet. Vivamus vel nulla risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse vel sodales ligula. Proin efficitur orci arcu, mattis pretium risus porta eget. Maecenas porttitor lacus tellus. Quisque nec dictum tortor. Etiam in est sagittis, congue lacus in, euismod magna. In ultricies fringilla felis, eu placerat eros facilisis vitae.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget metus quis purus congue dapibus. Integer ac tincidunt est. Integer porttitor accumsan dignissim. Integer vitae erat sed mi pulvinar viverra. Vivamus bibendum posuere egestas. Sed aliquet eros magna, at blandit eros lobortis sit amet. Vivamus vel nulla risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse vel sodales ligula. Proin efficitur orci arcu, mattis pretium risus porta eget. Maecenas porttitor lacus tellus. Quisque nec dictum tortor. Etiam in est sagittis, congue lacus in, euismod magna. In ultricies fringilla felis, eu placerat eros facilisis vitae.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget metus quis purus congue dapibus. Integer ac tincidunt est. Integer porttitor accumsan dignissim. Integer vitae erat sed mi pulvinar viverra. Vivamus bibendum posuere egestas. Sed aliquet eros magna, at blandit eros lobortis sit amet. Vivamus vel nulla risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse vel sodales ligula. Proin efficitur orci arcu, mattis pretium risus porta eget. Maecenas porttitor lacus tellus. Quisque nec dictum tortor. Etiam in est sagittis, congue lacus in, euismod magna. In ultricies fringilla felis, eu placerat eros facilisis vitae.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget metus quis purus congue dapibus. Integer ac tincidunt est. Integer porttitor accumsan dignissim. Integer vitae erat sed mi pulvinar viverra. Vivamus bibendum posuere egestas. Sed aliquet eros magna, at blandit eros lobortis sit amet. Vivamus vel nulla risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse vel sodales ligula. Proin efficitur orci arcu, mattis pretium risus porta eget. Maecenas porttitor lacus tellus. Quisque nec dictum tortor. Etiam in est sagittis, congue lacus in, euismod magna. In ultricies fringilla felis, eu placerat eros facilisis vitae.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget metus quis purus congue dapibus. Integer ac tincidunt est. Integer porttitor accumsan dignissim. Integer vitae erat sed mi pulvinar viverra. Vivamus bibendum posuere egestas. Sed aliquet eros magna, at blandit eros lobortis sit amet. Vivamus vel nulla risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse vel sodales ligula. Proin efficitur orci arcu, mattis pretium risus porta eget. Maecenas porttitor lacus tellus. Quisque nec dictum tortor. Etiam in est sagittis, congue lacus in, euismod magna. In ultricies fringilla felis, eu placerat eros facilisis vitae.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget metus quis purus congue dapibus. Integer ac tincidunt est. Integer porttitor accumsan dignissim. Integer vitae erat sed mi pulvinar viverra. Vivamus bibendum posuere egestas. Sed aliquet eros magna, at blandit eros lobortis sit amet. Vivamus vel nulla risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse vel sodales ligula. Proin efficitur orci arcu, mattis pretium risus porta eget. Maecenas porttitor lacus tellus. Quisque nec dictum tortor. Etiam in est sagittis, congue lacus in, euismod magna. In ultricies fringilla felis, eu placerat eros facilisis vitae.</p>
+`)
---
+++ src/with-template-source.ts
@@ -0,0 +1,21 @@
+import addons from '@storybook/addons';
+import { SET_TEMPLATE } from './globals';
+import Channel from '@storybook/channels';
+import { TemplateSourceOptions } from 'template-source-options';
+
+const Default: TemplateSourceOptions = {
+  pretty: true
+};
+
+export function withTemplateSource(source: string, options: Partial<TemplateSourceOptions> = Default): string {
+  if (options !== Default) {
+    for (let key in Default) {
+      if (!options.hasOwnProperty(key) && Default.hasOwnProperty(key)) {
+        (options as any)[key] = (Default as any)[key];
+      }
+    }
+  }
+  const channel: Channel = addons.getChannel();
+  channel.emit(SET_TEMPLATE, source, options);
+  return source;
+}
